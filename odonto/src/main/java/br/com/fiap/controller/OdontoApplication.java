1455c2e3c83284a8c0130a98d954110891a1ea7e
renan-b-eth <renanacademic21@gmail.com>
2024-10-01 19:17:31 -0300

    base 2 java e documentacao

--- odonto/src/main/java/br/com/fiap/controller/OdontoApplication.java
+++ odonto/src/main/java/br/com/fiap/controller/OdontoApplication.java
@@ -1,4 +1,4 @@
-package com.example.odonto;
+package br.com.fiap.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

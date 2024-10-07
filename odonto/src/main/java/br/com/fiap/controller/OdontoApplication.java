bcdde7a5668b26da8146cbfbf4cac532c515551c
renan-b-eth <renanacademic21@gmail.com>
2024-10-07 08:57:38 -0300

    login quase arrumado + criacao da documentacao endpoint

--- odonto/src/main/java/br/com/fiap/controller/OdontoApplication.java
+++ odonto/src/main/java/br/com/fiap/controller/OdontoApplication.java
@@ -17,42 +17,5 @@ public class OdontoApplication {
 		SpringApplication.run(OdontoApplication.class, args);
 	}
 
-	@RequestMapping("/hello")
-	public String index() {
-		return "<h1>Olá Mundo!</h1>";
-	}
 
-	// Conexão tipo GET HTTP
-	@RequestMapping("/cadastro/{nome}")
-	public String dizernome(@PathVariable String nome) {
-		return "Olá, meu nome é " + nome;
-	}
-
-	@PostMapping("/cadastrar")
-	public String cadastrar(@Validated @RequestBody Cadastro usuarioDto, BindingResult result) {
-		if (result.hasErrors()) {
-			// Retornar para a página de cadastro com os erros
-			return "ERRO";
-		}
-
-		Cadastro usuario = new Cadastro();
-		BeanUtils.copyProperties(usuarioDto, usuario);
-		usuario.setSenha(usuario.getSenha());
-
-
-		return "cadastro_sucesso";
-	}
-
-
-	// Conexão tipo GET HTTP
-	@RequestMapping("/info")
-	public String apresentar(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
-		return "<h1>Olá pessoal, meu nome é " + nome + " e eu tenho " + idade + " anos</h1>";
-	}
-
-	// Conexão tipo POST HTTP
-	@PostMapping("/postar")
-	public String postar() {
-		return "Objeto postado com sucesso.";
-	}
 }
--- odonto/src/main/java/br/com/fiap/controller/OdontoController2.java
+++ odonto/src/main/java/br/com/fiap/controller/OdontoController2.java
@@ -3,17 +3,26 @@ package br.com.fiap.controller;
 import br.com.fiap.beans.Cadastro;
 import br.com.fiap.beans.Login;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping("/odonto")
 public class OdontoController2 {
+
+	@PostMapping("/teste")
+	public ResponseEntity<String> myEndpoint(@RequestBody String request) {
+		// Processar a requisição
+		return ResponseEntity.ok("Requisicao recebida");
+	}
 	// Conexão tipo GET HTTP
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String index() {
-		return "<h1>Olá Mundo222!</h1>";
+		return "<h1>Olá Mundo!</h1>";
 	}
 
 	// Conexão tipo GET HTTP
@@ -31,22 +40,24 @@ public class OdontoController2 {
 
 		return "cadastro_sucesso";
 	}
-	@PostMapping("/login")
-	public String login(@Validated @RequestBody Login loginDto, BindingResult result) {
-		String emailPadrao = "adm@adm.com", senhaPadrao = "adm";
-		if (result.hasErrors()) {
-			return "<h1>DEU ERRO</h1>";
+	@GetMapping("/login")
+	public String login(@Validated @RequestBody String email, String senha) {
+		String emailPadrao = "adm", senhaPadrao = "adm";
+		System.out.println(emailPadrao);
+
+		if (emailPadrao.equals(email) && senhaPadrao.equals(senha)) {
+			return "redirect:/dashboard.html";
 		}
 
-		if (emailPadrao.equals(loginDto.getEmail()) && senhaPadrao.equals(loginDto.getSenha())) {
-			return "redirect:/dashboard";
-		}
+		/*if (emailPadrao.equals(loginDto.getEmail()) && senhaPadrao.equals(loginDto.getSenha())) {
+			return "redirect:/dashboard.html";
+		}*/
 
-		return "login";
+		return "<h1>LOGIN FEITO</h1>";
 	}
 	@PostMapping("/login/{email}/{senha}")
 	public String login(@Validated @RequestBody Login loginDto, @PathVariable String email, @PathVariable String senha, BindingResult result) {
-		String emailPadrao = "adm@adm.com", senhaPadrao = "adm";
+		String emailPadrao = "adm", senhaPadrao = "adm";
 		if (result.hasErrors()) {
 			return "<h1>DEU ERRO</h1>";
 		}
@@ -58,7 +69,7 @@ public class OdontoController2 {
 		return "login";
 	}
 	// Conexão tipo GET HTTP
-	@RequestMapping("/info")
+	@GetMapping("/info")
 	public String apresentar(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
 		return "<h1>Olá pessoal, meu nome é " + nome + " e eu tenho " + idade + " anos</h1>";
 	}

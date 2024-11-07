7a0ee954824b57efee4f59614b9bda30f55af649
renan-b-eth <renanacademic21@gmail.com>
2024-11-07 09:48:34 -0300

    arrumar, porem está quase certo

--- odonto/src/main/java/br/com/fiap/repository/ConsultorioRepository.java
+++ odonto/src/main/java/br/com/fiap/repository/ConsultorioRepository.java
@@ -1,10 +1,15 @@
 package br.com.fiap.repository;
 
+import br.com.fiap.beans.CadastroConsultorio;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsultorioRepository extends JpaRepository {
+public interface ConsultorioRepository extends JpaRepository<CadastroConsultorio, Long> {
+    @Query(value = "CALL cadastrar_clinica(:ID_CADASTROCLINICA, :nome, :email, :senha)", nativeQuery = true)
+    void cadastrarUsuario(@Param("ID_CADASTROCLINICA") int ID_CADASTROCLINICA, @Param("nome") String nome, @Param("email") String email, @Param("senha") String senha);
 
 }
